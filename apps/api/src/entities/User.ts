86af09fd8a4fe80f1d3a592b9e47fd4692813f15
afermanx <alexjferman@gmail.com>
2025-05-17 06:34:29 -0400

    feat: entities config

---
+++ apps/api/src/entities/User.ts
@@ -0,0 +1,17 @@
+import { Entity, Column } from 'typeorm';
+import { BaseEntity } from './BaseEntity';
+
+@Entity({ name: 'users' })
+export class User extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column({ nullable: true, name: 'google_id' })
+  googleId: string;
+}
