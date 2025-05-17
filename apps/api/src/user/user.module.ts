301fc2ae0151687e38a96a85a276268cf08679a7
afermanx <alexjferman@gmail.com>
2025-05-17 09:18:09 -0400

    feat: add gestão do usuario

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { User } from '../entities/User';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // <-- Necessário para injeção do Repository<User>
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService], // Opcional: útil se outro módulo (ex: AuthModule) precisar do UserService
 })
 export class UserModule {}
